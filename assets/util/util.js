96622e001c4ff4f1545139de854cebedfc9c28e2
Katia Velasquez <katia.velasquez@.com>
2022-11-03 22:33:36 -0300

    Cuarto commit: Implementacion de funcionalidad de paginador, esconder paginador cuando es solo 1 pagina, igualar select con lo escogido del menu lateral, boton adelante y atras

--- assets/util/util.js
+++ assets/util/util.js
@@ -6,39 +6,68 @@ function hideAndShow(id1) {
             div[i].style.display = 'none';
         } else if (div[i].classList.contains(id1)) {
             div[i].style.display = 'flex';
-            // if(id1!= 'all'){
-                // div[i].style.maxHeight='calc((100%/10)*7)';
-            // }
         }
     }
+    if(id1=='all'){
+        showPaginator();
+    }else{
+        hidePaginator();
+    };
+    sameSelection(id1);
 }
 // FILTRO PRODUCTOS EN SELECT
 function hideAndShowSelect() {
     let div = document.getElementsByClassName('productCard');
     let sel=document.getElementById('filter').value;
+    let main=document.getElementsByClassName('main');
+    let pager=document.getElementsByClassName("link");
     for (i = 0; i < div.length; i++) {
         if (!div[i].classList.contains(sel)) {
             div[i].style.display = 'none';
         } else if (div[i].classList.contains(sel)) {
             div[i].style.display = 'flex';
-            // if(id1!= 'all'){
-                // div[i].style.maxHeight='calc((100%/10)*7)';
-            // }
         }
     }
+    if(sel=='all'){
+        showPaginator();
+    }else{
+        hidePaginator();
+    };
+    main[0].scrollTop=0;
+}
+
+//MOSTRAR PAGINADOR
+function showPaginator(){
+    let paginator=document.getElementsByClassName("pager");
+    for(i=0; i< paginator[0].childElementCount; i++){
+        paginator[0].children[i].style.display='flex';
+    }
+}
+//ESCONDER PAGINADOR
+function hidePaginator(){
+    let paginator=document.getElementsByClassName("pager");
+    for(i=0; i< paginator[0].childElementCount; i++){
+        paginator[0].children[i].style.display='none';
+    }
+}
+//IGUALAR SELECCION DE MENU Y SELECT
+function sameSelection(seleccion){
+    let sel=document.getElementById('filter');
+    sel.value=seleccion;
 }
 
 //PAGINADOR
 function paginator(page){
     let productCards=document.getElementsByClassName('productCard');
     for (i = 0; i < productCards.length; i++) {
-        if ((page==1 && productCards[i].classList.contains('page2')) || (page==2 && !productCards[i].classList.contains('page2'))) {
+        if ((page==1 && productCards[i].classList.contains('page2')) || (page==2 && !productCards[i].classList.contains('page2')) || (page==0 && productCards[i].classList.contains('page2')) || (page==3 && !productCards[i].classList.contains('page2'))) {
             productCards[i].style.display = 'none';
         };
-        if ((page==1 && !productCards[i].classList.contains('page2')) || (page==2 && productCards[i].classList.contains('page2'))) {
+        if ((page==1 && !productCards[i].classList.contains('page2')) || (page==2 && productCards[i].classList.contains('page2')) || (page==0 && !productCards[i].classList.contains('page2')) || (page==3 && productCards[i].classList.contains('page2'))) {
             productCards[i].style.display = 'flex';
         };
-    }
+    };
+
 }
 
 
